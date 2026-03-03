39d9fc5f25b356f6d4c969edde30941cf082db54
Victor <victormarti2006@gmail.com>
2026-03-03 16:31:06 +0100

    Construccio del pare

---
+++ Criatura.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Peixera
+{
+    public enum Sexe { Mascle, Femella, Null }
+    public enum Direccio { Nord, Sud, Est, Oest }
+
+    public abstract class Criatura
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Sexe Genere { get; set; }
+        public Direccio Moviment { get; set; }
+        public bool EstaMort { get; set; } = false;
+
+        public Criatura(int x, int y, Sexe sexe, Direccio dir)
+        {
+            X = x;
+            Y = y;
+            Genere = sexe;
+            Moviment = dir;
+        }
+
+        public abstract void Moure(int midaTauler);
+    }
+}
