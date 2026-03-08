7517a050dbd472625d13585f193ce2eb5ee8c869
Victor <victormarti2006@gmail.com>
2026-03-08 17:46:02 +0100

    elaboracio del Pop

---
+++ Pop.cs
@@ -0,0 +1,41 @@
+namespace Peixera
+{
+    public class Pop : Criatura
+    {
+        public Pop(int x, int y) : base(x, y, Sexe.Null, Direccio.Est) 
+        {
+        }
+
+        public override void Moure(int mida)
+        {           
+            if (X == mida - 1 && Moviment == Direccio.Est)
+            {
+                Moviment = Direccio.Sud;
+            }
+            else if (Y == mida - 1 && Moviment == Direccio.Sud)
+            {
+                Moviment = Direccio.Oest;
+            }
+            else if (X == 0 && Moviment == Direccio.Oest)
+            {
+                Moviment = Direccio.Nord; 
+            }
+            else if (Y == 0 && Moviment == Direccio.Nord)
+            {
+                Moviment = Direccio.Est; 
+            }
+            ActualitzarPosicio();
+        }
+
+        private void ActualitzarPosicio()
+        {
+            switch (Moviment)
+            {
+                case Direccio.Nord: Y--; break;
+                case Direccio.Sud: Y++; break;
+                case Direccio.Est: X++; break;
+                case Direccio.Oest: X--; break;
+            }
+        }
+    }
+}
